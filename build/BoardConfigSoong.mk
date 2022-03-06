7c58f8c802145af47eb7866fec29b7ace0727c14
chaptsand <chaptsand@gmail.com>
2022-03-06 12:38:13 +0800

    proton: Add extended_compress_format soong var
    Change-Id: Ibd3844ddd37456e13ee0cd4936cd6dfb2c14f9ce

--- build/BoardConfigSoong.mk
+++ build/BoardConfigSoong.mk
@@ -7,3 +7,10 @@ TARGET_SURFACEFLINGER_UDFPS_LIB ?= surfaceflinger_udfps_lib
 
 # Soong value variables
 SOONG_CONFIG_aospGlobalVars_target_surfaceflinger_udfps_lib := $(TARGET_SURFACEFLINGER_UDFPS_LIB)
+
+SOONG_CONFIG_NAMESPACES += aospQcomVars
+SOONG_CONFIG_aospQcomVars += \
+    supports_extended_compress_format
+
+# Soong value variables
+SOONG_CONFIG_aospQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)
